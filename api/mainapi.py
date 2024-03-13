46b89d5a3f9e6e2b57949df0395b59f0bece5fca
Stolenvw <51084246+stolenvw@users.noreply.github.com>
2024-03-13 16:12:49 -0400

    0.0.2 release

--- api/mainapi.py
+++ api/mainapi.py
@@ -19,7 +19,7 @@ load_dotenv(dotenv_path=".env")
 app = FastAPI(
     title="Palworld API",
     description=D.description,
-    version="0.0.1",
+    version="0.0.2",
     contact={
         "name": "pyPalworldAPI GitHub",
         "url": "https://github.com/stolenvw/pyPalworldAPI",
