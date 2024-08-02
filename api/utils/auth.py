24ccc5554c035f60285d10d2aa21ebdcf23b19b3
Stolenvw <51084246+stolenvw@users.noreply.github.com>
2024-08-02 16:07:02 -0400

    fix for server error when no refresh token was found for a user

--- api/utils/auth.py
+++ api/utils/auth.py
@@ -85,7 +85,7 @@ async def verify_refresh_token(db, token: str):
         raise credential_exception
     else:
         token_valid = await AuthQuery.check_refresh_token(db, username)
-        if token != token_valid.refresh_token:
+        if not token_valid or token != token_valid.refresh_token:
             token_invalid = HTTPException(
                 status_code=status.HTTP_400_BAD_REQUEST,
                 detail="Invalid refresh token",
